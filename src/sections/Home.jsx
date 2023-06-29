9da6142d6265e6a7ee2e12e00f2349612286904b
Devaraj Rangannan <devarangan2002@gmail.com>
2023-06-29 17:46:00 +0530

    Home Page Successfully created

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import { CONTAINER, md_CONTAINER } from './styles/Home'
+import React, {useState} from 'react'
+import { CONTAINER, md_CONTAINER, MAIN_SECTION, md_MAIN_SECTION, PROFILE, md_PROFILE, TEXT_CONTAINER , md_TEXT_CONTAINER, NAME, md_NAME, DOAMIN, md_DOAMIN, RESUME_BTN, md_RESUME_BTN, WAVE, md_WAVE} from './styles/Home'
+import axios from 'axios'
+import profile from "../asserts/profile.png"
+
+
 
 export default function Home() {
+  const URL = process.env.REACT_APP_METADATA_RESUME
+
+  const [resumeURL, setresumeURL] = useState("")
+  
+  const getData = async()=>{
+    try{
+      const response = await axios.get(URL);
+      const data = await response.data
+      setresumeURL(data.URL)
+      console.log(data.URL);
+    }
+    catch(e){
+      console.log(e);
+    }
+
+  }
+
+  getData()
+
+
   return (
     <>
         <div id='home' className={`${CONTAINER} ${md_CONTAINER}`}>
-            Home
+          <div className={`${MAIN_SECTION} ${md_MAIN_SECTION}  `}>
+            <img className={`${PROFILE} ${md_PROFILE}`} src={profile} alt="profilePic" />
+            <div className={`${TEXT_CONTAINER} ${md_TEXT_CONTAINER}`}>
+              <div className={`${NAME} ${md_NAME}`}>Devaraj Rangannan</div>
+              <div className={`${DOAMIN}, ${md_DOAMIN}`}>Full Stack Developer</div>
+              <a href={resumeURL} target='blank'>
+                <button className={`${RESUME_BTN} ${md_RESUME_BTN}`}>{resumeURL===""?"Loading...":"Resume Download"}</button>
+              </a>
+            </div>
+          </div>
+            <div className={`${WAVE} ${md_WAVE}`}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path  fillOpacity="1" d="M0,160L40,144C80,128,160,96,240,85.3C320,75,400,85,480,112C560,139,640,181,720,176C800,171,880,117,960,128C1040,139,1120,213,1200,229.3C1280,245,1360,203,1400,181.3L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+          </div>
         </div>
-    </>
-    
+      </>
   )
 }
--- src/sections/styles/Home.js
+++ src/sections/styles/Home.js
@@ -1,2 +1,23 @@
-export const CONTAINER = "h-96 SECTIONS w-full bg-[#2E2E2E]   text-red-500 flex items-center justify-center text-5xl"
+export const CONTAINER = "relative md:scroll-mt-16  SECTIONS w-full bg-[#2E2E2E] items-center justify-center "
 export const md_CONTAINER = "md:h-[42rem]"
+
+export const MAIN_SECTION = "w-[85%] mx-auto "
+export const md_MAIN_SECTION = "md:flex md:items-center md:h-[85%] "
+
+export const PROFILE = "pt-10  mx-auto  relative h-[25rem] "
+export const md_PROFILE = "lg:w-[30%] md:pt-0 md:m-0"
+
+export const TEXT_CONTAINER = "text-center t font-bold text-white"
+export const md_TEXT_CONTAINER = "md:ml-10 md:text-left"
+
+export const NAME = "text-3xl font-bold lg:text-7xl"
+export const md_NAME = "md:text-5xl"
+
+export const DOAMIN = "text-xl font-bold mt-2 mb-2  "
+export const md_DOAMIN = "lg:text-3xl md:my-4 md:my-4"
+
+export const RESUME_BTN = "w-44 p-3 text-black font-semibold bg-white rounded-lg mb-10  hover:bg-gray-300   "
+export const md_RESUME_BTN = "md:mb-0"
+
+export const WAVE = "w-full bottom-0 fill-white"
+export const md_WAVE = "md:absolute"
