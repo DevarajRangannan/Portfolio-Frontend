a812cc0b56c018f27d199271c2f7af4e6e6b4556
Devaraj Rangannan <devarangan2002@gmail.com>
2023-07-12 16:17:38 +0530

    Contact section successfully implemented

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { CONTAINER, md_CONTAINER } from './styles/Contact'
+import { CONTAINER, md_CONTAINER, TITLE, BODY_CONTAINER, EMAIL_ADDRESS, SUB_BODY_CONTAINER, LOGO_CONTAINER } from './styles/Contact'
 
 export default function Contact() {
   return (
     <div id='contact' className={`${CONTAINER} ${md_CONTAINER}`}>
-        Contact
+        
+        <h1 className={TITLE}>Contact</h1>
+
+        <div className={BODY_CONTAINER}>
+            <h1 className={EMAIL_ADDRESS} >devarangan2002@gmail.com</h1>
+
+            <div className={SUB_BODY_CONTAINER}>
+                <a  href="https://in.linkedin.com/in/devaraj-rangannan" target="_blank" rel="noreferrer"><img src="https://devarajrangannan.github.io/Images/LinkedIn_logo.png" alt="LinkedIn_logo" className={LOGO_CONTAINER}/></a>
+
+                <a  href="mailto:devarangan2002@gmail.com" target="_blank" rel="noreferrer"><img src="https://devarajrangannan.github.io/Images/Gmail_logo.png" alt="Gmail_logo" className={LOGO_CONTAINER}/></a>
+
+                <a  href="https://github.com/DevarajRangannan" target="_blank" rel="noreferrer"><img src="https://devarajrangannan.github.io/Images/Git_logo.png" alt="Github_logo" className={LOGO_CONTAINER}/></a>
+
+                <a  href="https://www.hackerrank.com/Deva_Rangan" target="_blank" rel="noreferrer"><img src="https://deva-rangan-test.s3.ap-south-1.amazonaws.com/HackerRank_logo.png" alt="CodeChef_logo" className={LOGO_CONTAINER}/></a>
+            </div>
+        </div>
+        
     </div>
   )
 }
--- src/sections/styles/Contact.js
+++ src/sections/styles/Contact.js
@@ -1,2 +1,12 @@
-export const CONTAINER = "h-96 SECTIONS w-full bg-[#2E2E2E]   text-red-500 flex items-center justify-center text-5xl"
-export const md_CONTAINER = "md:h-[42rem]"
+export const CONTAINER = "px-10 py-5 md:pt-5 bg-[#2E2E2E] "
+export const md_CONTAINER = " md:px-24 md:pb-12"
+
+export const TITLE = "text-5xl font-bold text-center text-white"
+
+export const BODY_CONTAINER = "flex flex-col justify-center items-center"
+
+export const EMAIL_ADDRESS = "mt-10 md:mt-20 text-xl md:text-5xl font-bold text-center text-white"
+
+export const SUB_BODY_CONTAINER = "w-full  md:w-1/2 mt-10 flex justify-between"
+
+export const LOGO_CONTAINER = "w-8 h-8 md:w-12 md:h-12 rounded"
