aac9744592ef426eb98ce303c137d69dcdf7f6f0
Devaraj Rangannan <devarangan2002@gmail.com>
2023-07-12 08:41:34 +0530

    Education section successfully implemented

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-import { CONTAINER, md_CONTAINER } from './styles/Education'
+import { CONTAINER, md_CONTAINER, TITLE, MAIN_BODY, EDUCATION_CONTAINER, COURSE_CONTAINER, COURSE_TITLE, COURSE_DES, COURSE_MARK } from './styles/Education'
+import Wave  from "../asserts/wave-black.svg"
 
 export default function Education() {
   return (
-    <div id='education' className={`${CONTAINER} ${md_CONTAINER}`}>
-        Education
-    </div>
+      <div id='education' className={`${CONTAINER} ${md_CONTAINER} `}>
+          <h1 className={TITLE}>Education</h1>
+          <div className={MAIN_BODY}>
+              <div className={EDUCATION_CONTAINER}>
+                  <div className={COURSE_CONTAINER}>
+                      <h2 className={COURSE_TITLE}>DEGREE</h2>
+                      <h3 className={COURSE_DES}>Bachelor Of Cumputer Application ( 2020-2023 )</h3>
+                      <h4 className={COURSE_MARK}>Mark : 75%</h4>
+                  </div>
+
+                  <div className={COURSE_CONTAINER}>
+                      <h2 className={COURSE_TITLE}>12 <sup>Th</sup></h2>
+                      <h3 className={COURSE_DES}>State Board ( 2018-2019 )</h3>
+                      <h4 className={COURSE_MARK}>Mark : 52%</h4>
+                  </div>
+
+                  <div className={COURSE_CONTAINER}>
+                      <h2 className={COURSE_TITLE}>11<sup> Th</sup></h2>
+                      <h3 className={COURSE_DES}>State Board ( 2017-2018 )</h3>
+                      <h4 className={COURSE_MARK}>Mark : 48%</h4>
+                  </div>
+
+                  <div className={COURSE_CONTAINER}>
+                      <h2 className={COURSE_TITLE}>10<sup> Th</sup></h2>
+                      <h3 className={COURSE_DES}>State Board ( 2016-2017 )</h3>
+                      <h4 className={COURSE_MARK}>Mark : 82.5%</h4>
+                  </div>
+
+              </div>
+
+          </div>
+
+          <div className={`h-[10rem]  bg-cover	`} style={{backgroundImage:`url(${Wave})`}}></div>
+
+      </div>
   )
 }
--- src/sections/styles/Education.js
+++ src/sections/styles/Education.js
@@ -1,2 +1,17 @@
-export const CONTAINER = "h-96 SECTIONS w-full bg-white text-red-500 flex items-center justify-center text-5xl"
-export const md_CONTAINER = "md:h-[42rem]"
+export const CONTAINER = "pt-5 relative md:scroll-mt-16"
+export const md_CONTAINER = " "
+
+export const TITLE = "text-5xl font-bold text-center"
+export const MAIN_BODY = "md:px-24 lg:bg-[right_bottom_1rem]  xl:bg-[length:600px_400px]  xl:bg-no-repeat xl:bg-[url('https://deva-rangan-test.s3.ap-south-1.amazonaws.com/education_bg.jpg')] pl-8 pt-8 "
+
+
+export const EDUCATION_CONTAINER = "border-l-8 border-red-500 pt-10 px-10 rounded-xl relative "
+
+export const COURSE_CONTAINER = "mb-10 brfore:content-[''] before:h-6 before:w-6 before:bg-white before:absolute before:-left-4 before:rounded-2xl before:border-4 before:border-red-500"
+export const COURSE_TITLE = "text-2xl font-bold "
+export const COURSE_DES = "text-xl font-medium text-gray-800"
+export const COURSE_MARK = "text-lg font-medium text-gray-600"
+
+
+
+
